aed7eaa94bac9eb3cd2f8b27154e6cf01a521ccf
Ralf Baechle <ralf@linux-mips.org>
2020-03-12 17:41:23 +0800

    BACKPORT: FROMLIST: MIPS: Add syscall auditing support
    The original patch is from Ralf. I have maintained it for more than six
    years on Loongson platform, and it works perfectly. Most of the commit
    messages are written by Ralf.
    
    MIPS doesn't quite fit into the existing pattern of other architectures
    and I'd appreciate your comments and maybe even an Acked-by.
    
     - Linux on MIPS extends the traditional syscall table used by older UNIX
       implementations.  This is why 32-bit Linux syscalls are starting from
       4000; the native 64-bit syscalls start from 5000 and the N32 compat ABI
       from 6000.  The existing syscall bitmap is only large enough for at most
       2048 syscalls, so I had to increase AUDIT_BITMASK_SIZE to 256 which
       provides enough space for 8192 syscalls.  Because include/uapi/linux/
       audit.h and AUDIT_BITMASK_SIZE are exported to userspace I've used an
       #ifdef __mips__ for this.
    
     - The code treats the little endian MIPS architecture as separate from
       big endian.  Combined with the 3 ABIs that's 6 combinations.  I tried
       to sort of follow the example set by ARM which explicitly lists the
       (rare) big endian architecture variant - but it doesn't seem to very
       useful so I wonder if this could be squashed to just the three ABIs
       without consideration of endianess?
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: Huacai Chen <chenhc@lemote.com>
    
    Link: https://patch.msgid.link/1584006083-28936-1-git-send-email-chenhc@lemote.com
    [ Kimi Code: context shifted in arch/mips/Kconfig and kernel/auditsc.c;
      the posting's literal 'case 6:' duplicates AUDITSC_OPENAT2 (value 6) in the
      current auditsc_class_t enum and its arch/mips/include/asm/unistd.h hunk
      redefines the existing NR_syscalls, so on reapplication to v7.3-rc1 the
      syscall-classify helpers use the AUDITSC_* constants with AUDITSC_MIPS_N32
      added to include/linux/audit_arch.h; the duplicate NR_syscalls block and
      the posting's stale 'select SYSVIPC_COMPAT if SYSVIPC' context lines
      (absent from v7.3-rc1) were dropped ]
    Signed-off-by: Kexy Biscuit <kexybiscuit@aosc.io>

---
+++ arch/mips/kernel/audit-native.c
@@ -0,0 +1,79 @@
+#include <linux/init.h>
+#include <linux/types.h>
+#include <linux/audit.h>
+#include <asm/unistd.h>
+
+static unsigned dir_class[] = {
+#include <asm-generic/audit_dir_write.h>
+~0U
+};
+
+static unsigned read_class[] = {
+#include <asm-generic/audit_read.h>
+~0U
+};
+
+static unsigned write_class[] = {
+#include <asm-generic/audit_write.h>
+~0U
+};
+
+static unsigned chattr_class[] = {
+#include <asm-generic/audit_change_attr.h>
+~0U
+};
+
+static unsigned signal_class[] = {
+#include <asm-generic/audit_signal.h>
+~0U
+};
+
+
+/*
+ * Pretend to be a single architecture
+ */
+int audit_classify_arch(int arch)
+{
+	return 0;
+}
+
+extern int audit_classify_syscall_o32(int abi, unsigned syscall);
+extern int audit_classify_syscall_n32(int abi, unsigned syscall);
+
+int audit_classify_syscall(int abi, unsigned syscall)
+{
+	switch (syscall) {
+	case __NR_open:
+		return AUDITSC_OPEN;
+	case __NR_openat:
+		return AUDITSC_OPENAT;
+#ifdef __NR_socketcall		/* Only exists on O32 */
+	case __NR_socketcall:
+		return AUDITSC_SOCKETCALL;
+#endif
+	case __NR_execve:
+		return AUDITSC_EXECVE;
+	default:
+		if (IS_ENABLED(CONFIG_AUDITSYSCALL_O32) &&
+		    (abi == AUDIT_ARCH_MIPS || abi == AUDIT_ARCH_MIPSEL))
+			return audit_classify_syscall_o32(abi, syscall);
+		if (IS_ENABLED(CONFIG_AUDITSYSCALL_N32) &&
+		    (abi == AUDIT_ARCH_MIPS64N32 ||
+		     abi == AUDIT_ARCH_MIPSEL64N32))
+			return audit_classify_syscall_n32(abi, syscall);
+		return AUDITSC_NATIVE;
+	}
+}
+
+static int __init audit_classes_init(void)
+{
+	audit_register_class(AUDIT_CLASS_WRITE, write_class);
+	audit_register_class(AUDIT_CLASS_READ, read_class);
+	audit_register_class(AUDIT_CLASS_DIR_WRITE, dir_class);
+	audit_register_class(AUDIT_CLASS_CHATTR, chattr_class);
+	audit_register_class(AUDIT_CLASS_SIGNAL, signal_class);
+
+	return 0;
+}
+
+__initcall(audit_classes_init);
