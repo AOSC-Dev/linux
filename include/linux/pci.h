2c3a219cda88d07588e5cad9b939cbb6eb1f717b
Maximilian Luz <luzmaximilian@gmail.com>
2023-02-19 22:12:24 +0100

    BACKPORT: SURFACE: PCI: Add quirk to prevent calling shutdown method
    Work around buggy EFI firmware: On some Microsoft Surface devices
    (Surface Pro 9 and Surface Laptop 5) the EFI ResetSystem call with
    EFI_RESET_SHUTDOWN doesn't function properly. Instead of shutting the
    system down, it returns and the system stays on.
    
    It turns out that this only happens after PCI shutdown callbacks ran for
    specific devices. Excluding those devices from the shutdown process
    makes the ResetSystem call work as expected.
    
    TODO: Maybe we can find a better way or the root cause of this?
    
    Not-Signed-off-by: Maximilian Luz <luzmaximilian@gmail.com>
    Patchset: surface-shutdown
    
    [ Mingcong Bai: Resolved a minor post-7.0 merge conflict in
      include/linux/pci.h ]
    
    Link: https://github.com/linux-surface/linux-surface/tree/4c136ec928ce0b458562c7533852db67a8e0dc91
    Signed-off-by: Mingcong Bai <jeffbai@aosc.io>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -514,6 +514,7 @@ struct pci_dev {
 	unsigned int	rom_attr_enabled:1;	/* Display of ROM attribute enabled? */
 	unsigned int	non_mappable_bars:1;	/* BARs can't be mapped by CPU or peers */
 	unsigned int	aspm_os_control:1;	/* Display of ROM attribute enabled? */
+	unsigned int	no_shutdown:1;		/* Do not touch device on shutdown */
 	pci_dev_flags_t dev_flags;
 	atomic_t	enable_cnt;	/* pci_enable_device has been called */
 
